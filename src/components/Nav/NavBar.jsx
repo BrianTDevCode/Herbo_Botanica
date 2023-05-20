b25eb10bbf522a1fb5c3a5013fb1c6ec6ae915c7
MatiasRicci <matricc89@gmail.com>
2023-05-20 01:49:37 -0300

    Ultima actualizacion Navbar
    Se agrega hover y subrayado

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,53 +1,14 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import "./NavBar.css";
+
 import LOGO from "../../assets/logo.png";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const NavBar = () => {
-  const [showHerboPetsSubcategories, setShowHerboPetsSubcategories] = useState(false);
-  const [showHermoHomeSubcategories, setShowHermoHomeSubcategories] = useState(false);
-  const [showSahumosSubcategories, setShowSahumosSubcategories] = useState(false);
-  const [showFormulasFloralSubcategories, setShowFormulasFloralSubcategories] = useState(false);
-  const [showAromaterapiasSubcategories, setShowAromaterapiasSubcategories] = useState(false);
+  const activeCategoryRef = useRef("");
 
-  const handleHerboPetsSubcategoriesEnter = () => {
-    setShowHerboPetsSubcategories(true);
-  };
-
-  const handleHerboPetsSubcategoriesLeave = () => {
-    setShowHerboPetsSubcategories(false);
-  };
-
-  const handleHermoHomeSubcategoriesEnter = () => {
-    setShowHermoHomeSubcategories(true);
-  };
-
-  const handleHermoHomeSubcategoriesLeave = () => {
-    setShowHermoHomeSubcategories(false);
-  };
-
-  const handleSahumosSubcategoriesEnter = () => {
-    setShowSahumosSubcategories(true);
-  };
-
-  const handleSahumosSubcategoriesLeave = () => {
-    setShowSahumosSubcategories(false);
-  };
-
-  const handleFormulasFloralSubcategoriesEnter = () => {
-    setShowFormulasFloralSubcategories(true);
-  };
-
-  const handleFormulasFloralSubcategoriesLeave = () => {
-    setShowFormulasFloralSubcategories(false);
-  };
-
-  const handleAromaterapiasSubcategoriesEnter = () => {
-    setShowAromaterapiasSubcategories(true);
-  };
-
-  const handleAromaterapiasSubcategoriesLeave = () => {
-    setShowAromaterapiasSubcategories(false);
+  const handleCategoryClick = (category) => {
+    activeCategoryRef.current = activeCategoryRef.current === category ? "" : category;
   };
 
   return (
@@ -56,80 +17,67 @@ const NavBar = () => {
         <figure className="header__figure">
           <img className="header__img" src={LOGO} alt="logo" />
         </figure>
+
         <div className="header__cart">
           <ShoppingCartIcon className="header__cart-icon" />
         </div>
       </div>
+
       <nav className="header__nav">
         <ul className="header__ul">
           <li
-            className={`header__li ${showHerboPetsSubcategories ? "show-subcategories" : ""}`}
-            onMouseEnter={handleHerboPetsSubcategoriesEnter}
-            onMouseLeave={handleHerboPetsSubcategoriesLeave}
+            className={`header__li ${activeCategoryRef.current === "Herbo Pets" ? "active" : ""}`}
+            onClick={() => handleCategoryClick("Herbo Pets")}
           >
             Herbo Pets
-            {showHerboPetsSubcategories && (
-              <ul className="header__sub-ul">
-                <li className="header__sub-li">Formulas Florales</li>
-                <li className="header__sub-li">Brumas</li>
-              </ul>
-            )}
+            <ul className="header__sub-ul">
+              <li className="header__sub-li">Formulas Florales</li>
+              <li className="header__sub-li">Brumas</li>
+            </ul>
           </li>
           <li
-            className={`header__li ${showHermoHomeSubcategories ? "show-subcategories" : ""}`}
-            onMouseEnter={handleHermoHomeSubcategoriesEnter}
-            onMouseLeave={handleHermoHomeSubcategoriesLeave}
-          >
-            Hermo Home
-            {showHermoHomeSubcategories && (
-              <ul className="header__sub-ul">
-                <li className="header__sub-li">Sales de baño</li>
-                <li className="header__sub-li">Difusores</li>
-                <li className="header__sub-li">Esencias para hornillos</li>
-              </ul>
-            )}
-          </li>
-          <li
-            className={`header__li ${showSahumosSubcategories ? "show-subcategories" : ""}`}
-            onMouseEnter={handleSahumosSubcategoriesEnter}
-            onMouseLeave={handleSahumosSubcategoriesLeave}
+            className={`header__li ${activeCategoryRef.current === "Sahumos" ? "active" : ""}`}
+            onClick={() => handleCategoryClick("Sahumos")}
           >
             Sahumos
-            {showSahumosSubcategories && (
-              <ul className="header__sub-ul">
-                <li className="header__sub-li">Sahumerios</li>
-                <li className="header__sub-li">Carbones de defumación</li>
-                <li className="header__sub-li">Velas</li>
-              </ul>
-            )}
+            <ul className="header__sub-ul">
+              <li className="header__sub-li">Sahumerios</li>
+              <li className="header__sub-li">Carbones de defumacion</li>
+              <li className="header__sub-li">Velas</li>
+            </ul>
           </li>
           <li
-            className={`header__li ${showFormulasFloralSubcategories ? "show-subcategories" : ""}`}
-            onMouseEnter={handleFormulasFloralSubcategoriesEnter}
-            onMouseLeave={handleFormulasFloralSubcategoriesLeave}
+            className={`header__li ${activeCategoryRef.current === "Hermo Home" ? "active" : ""}`}
+            onClick={() => handleCategoryClick("Hermo Home")}
           >
-            Formulas Floral
-            {showFormulasFloralSubcategories && (
-              <ul className="header__sub-ul">
-                <li className="header__sub-li">Tónicos herbales</li>
-              </ul>
-            )}
+            Hermo Home
+            <ul className="header__sub-ul">
+              <li className="header__sub-li">Sales de baño</li>
+              <li className="header__sub-li">Difusores</li>
+              <li className="header__sub-li">Esencias para hornillos</li>
+            </ul>
           </li>
           <li
-            className={`header__li ${showAromaterapiasSubcategories ? "show-subcategories" : ""}`}
-            onMouseEnter={handleAromaterapiasSubcategoriesEnter}
-            onMouseLeave={handleAromaterapiasSubcategoriesLeave}
+            className={`header__li ${activeCategoryRef.current === "Formula Floral" ? "active" : ""}`}
+            onClick={() => handleCategoryClick("Formula Floral")}
+          >
+            Formula Floral
+            <ul className="header__sub-ul">
+              <li className="header__sub-li">Tónicos herbales</li>
+            </ul>
+          </li>
+          <li
+            className={`header__li ${activeCategoryRef.current === "Aromaterapias" ? "active" : ""}`}
+            onClick={() => handleCategoryClick("Aromaterapias")}
           >
             Aromaterapias
-            {showAromaterapiasSubcategories && (
-              <ul className="header__sub-ul">
-                <li className="header__sub-li">Body Oils</li>
-                <li className="header__sub-li">Brumas Auricas</li>
-                <li className="header__sub-li">Roll On</li>
-                <li className="header__sub-li">Aceites esenciales</li>
-                <li className="header__sub-li">Unguento</li>
-              </ul>
-            )}
+            <ul className="header__sub-ul">
+              <li className="header__sub-li">Body Oils</li>
+              <li className="header__sub-li">Brumas Auricas</li>
+              <li className="header__sub-li">Roll On</li>
+              <li className="header__sub-li">Aceites esenciales</li>
+              <li className="header__sub-li">Unguento</li>
+            </ul>
           </li>
         </ul>
       </nav>
