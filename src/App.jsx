fbe034cf5235314c908a17c804e1b105c0ef498d
BrianTDevCode <briantriasm@gmail.com>
2023-05-22 21:46:36 -0300

    slider v1
    primera version slider homePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import NavBar from "./components/Nav/NavBar"
 import { HomePage } from "./components/HomePage/HomePage"
-import { HelpInfo } from "./components/HelpInfo/HelpInfo"
+
+
 
 
 
 const App = () => {
   return (
     <>
+
     <NavBar/>
-    <HomePage/>
-    <HelpInfo/>
+    <HomePage/>    
     </>
   )
 }
---
+++ src/components/CarouselHomePage/CarouselHomePage.jsx
@@ -0,0 +1,46 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import banner1 from '../../assets/banner1.png'
+import banner2 from '../../assets/banner2.png'
+import banner3 from '../../assets/banner3.png'
+import banner4 from '../../assets/banner4.png'
+export const CarouselHomePage = () => {
+
+
+  return (
+    <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active" data-bs-interval="5000">
+    <figure className='banner__figure'>
+      <img src={banner1} className="d-block w-100 banner__img" alt="banner"/>
+    </figure>
+    </div>
+    <div className="carousel-item" data-bs-interval="5000">
+    <figure className='banner__figure'>
+      <img src={banner2} className="d-block w-100 banner__img" alt="banner"/>
+    </figure>
+    </div>
+    <div className="carousel-item" data-bs-interval="5000">
+    <figure className='banner__figure'>
+      <img src={banner3} className="d-block w-100 banner__img" alt="banner"/>
+    </figure>
+    </div>
+
+    <div className="carousel-item"data-bs-interval="5000">
+    <figure className='banner__figure'>
+      <img src={banner4} className="d-block w-100 banner__img" alt="banner"/>
+    </figure>
+    </div>
+
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+  );
+}
