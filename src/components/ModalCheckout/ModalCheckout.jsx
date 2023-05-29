4b2e55d949a8c77e9af707629da0521f3af75c89
MatiasRicci <matricc89@gmail.com>
2023-05-29 20:14:32 -0300

    validacion de formulario
    Validacion de formulario

--- src/components/ModalCheckout/ModalCheckout.jsx
+++ src/components/ModalCheckout/ModalCheckout.jsx
@@ -1,8 +1,8 @@
-import './ModalCheckout.css'
+import './ModalCheckout.css';
 import logo from "../../assets/Logo_blanco.png";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-
 
 export const ModalCheckout = () => {
   return (
@@ -11,58 +11,121 @@ export const ModalCheckout = () => {
         <figure className="checkout__figure">
           <img className="checkout__img" src={logo} alt="logo" />
         </figure>
-
         <p className="checkout__title">Información de contacto</p>
       </div>
-      <form className="checkout__frm">
-
-       <input className="frm__input" type="text" placeholder="Correo electrónico"/>
-      
-
-    <p className="frm__label">FORMA DE ENTREGA</p>
-      
-    <div className='frm__radio'>
-      <div className="frm__opcion">
-
-      <input type="radio" id="envio" name="envio" value="envio"/>
-      <ShoppingCartIcon  style={{maxWidth: '40', maxHeight: '40px', minWidth: '40px', minHeight: '40px'}} className='frm__icon'/>
-      <label>Envío</label>
-      </div>
-    
-      <div className="frm__opcion">
-
-      <input type="radio" id="retiro" name="retiro" value="retiro"/>
-      <LocalShippingIcon style={{maxWidth: '40', maxHeight: '40px', minWidth: '40px', minHeight: '40px'}} className='frm__icon'/>
-      <label>Retiro</label>
-      </div>
-    </div>
-
-    <p className="frm__label">Dirección de envíó</p>
-    <input className="frm__input" type="text" placeholder="País"/>
-
-    <div className="frm__group">
-    <input className="frm__input" type="text" placeholder="Nombre"/>
-    <input className="frm__input" type="text" placeholder="Apellido"/>
-    </div>
-
-
-
-    <div className="frm__group">
-    <input className="frm__input" type="text" placeholder="Dirección"/>
-    <input className="frm__input" type="text" placeholder="Número"/>
-    </div>
-
-    <input className="frm__input" type="text" placeholder="Apartamento, local, etc (opcional)"/>
-
-    <div className="frm__group">
-    <input className="frm__input" type="text" placeholder="Código postal"/>
-    <input className="frm__input" type="text" placeholder="Ciudad"/>
-    </div>
-
-    <input className="frm__input" type="text" placeholder="Teléfono"/>
-
-      <button className='frm__btn'>Enviár Formulario</button>
-      </form>
+      <Formik
+        initialValues={{
+          email: '',
+          deliveryMethod: 'envio',
+          country: '',
+          firstName: '',
+          lastName: '',
+          address: '',
+          apartment: '',
+          postalCode: '',
+          city: '',
+          phone: '',
+        }}
+        onSubmit={values => {
+          console.log(values);
+        }}
+      >
+        <Form className="checkout__frm">
+          <Field
+            className="frm__input"
+            type="text"
+            name="email"
+            placeholder="Correo electrónico"
+          />
+          <div className="frm__label">FORMA DE ENTREGA</div>
+          <div className="frm__radio">
+            <div className="frm__opcion">
+              <Field
+                type="radio"
+                id="envio"
+                name="deliveryMethod"
+                value="envio"
+              />
+              <ShoppingCartIcon className="frm__icon" />
+              <label htmlFor="envio">Envío</label>
+            </div>
+            <div className="frm__opcion">
+              <Field
+                type="radio"
+                id="retiro"
+                name="deliveryMethod"
+                value="retiro"
+              />
+              <LocalShippingIcon className="frm__icon" />
+              <label htmlFor="retiro">Retiro</label>
+            </div>
+          </div>
+          <div className="frm__label">Dirección de envío</div>
+          <Field
+            className="frm__input"
+            type="text"
+            name="country"
+            placeholder="País"
+          />
+          <div className="frm__group">
+            <Field
+              className="frm__input"
+              type="text"
+              name="firstName"
+              placeholder="Nombre"
+            />
+            <Field
+              className="frm__input"
+              type="text"
+              name="lastName"
+              placeholder="Apellido"
+            />
+          </div>
+          <div className="frm__group">
+            <Field
+              className="frm__input"
+              type="text"
+              name="address"
+              placeholder="Dirección"
+            />
+            <Field
+              className="frm__input"
+              type="text"
+              name="apartment"
+              placeholder="Número"
+            />
+          </div>
+          <Field
+            className="frm__input"
+            type="text"
+            name="apartment"
+            placeholder="Apartamento, local, etc (opcional)"
+          />
+          <div className="frm__group">
+            <Field
+              className="frm__input"
+              type="text"
+              name="postalCode"
+              placeholder="Código postal"
+            />
+            <Field
+              className="frm__input"
+              type="text"
+              name="city"
+              placeholder="Ciudad"
+            />
+          </div>
+          <Field
+            className="frm__input"
+            type="text"
+            name="phone"
+            placeholder="Teléfono"
+          />
+          <button className="frm__btn" type="submit">
+            Enviar Formulario
+          </button>
+        </Form>
+      </Formik>
     </section>
   );
 };
