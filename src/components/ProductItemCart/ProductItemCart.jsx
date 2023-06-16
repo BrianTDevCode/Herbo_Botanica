060d7797e9879f503c713e3dceb217f59931365f
MatiasRicci <matricc89@gmail.com>
2023-06-15 21:49:13 -0300

    ProductItemCart
    probando

--- src/components/ProductItemCart/ProductItemCart.jsx
+++ src/components/ProductItemCart/ProductItemCart.jsx
@@ -1,5 +1,4 @@
 import "./ProductItemCart.css";
-
 import imgCard from "../../assets/pruebaCard.png";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
@@ -8,33 +7,29 @@ import { useContext, useState } from "react";
 import { CartContext } from "../../context/CartContext";
 
 export const ProductItemCart = ({ data }) => {
-  const { items, cleanCart, removeItem } = useContext(CartContext);
+  const { items, setItems, removeItem } = useContext(CartContext);
 
-  data.cantidad = [];
+  const [quantity, setQuantity] = useState(data.cantidad || 1);
 
-  let [quantity, setQuantity] = useState(1);
-  let [product, setProduct] = useState(data);
+  const updateQuantity = (newQuantity) => {
+    const updatedItems = items.map((item) =>
+      item.id === data.id ? { ...item, cantidad: newQuantity } : item
+    );
+    setItems(updatedItems);
+  };
 
   const handleDecrementQuantity = () => {
     if (quantity > 1) {
-        
-      setQuantity((prevConnt) => prevConnt - 1);
-      data.cantidad = quantity;
-      setProduct(data);
-      console.log(data);
+      const newQuantity = quantity - 1;
+      setQuantity(newQuantity);
+      updateQuantity(newQuantity);
     }
   };
 
   const handleIncrementQuantity = () => {
-      //quantity++;
-      setQuantity((prevConnt) => prevConnt + 1);
-      data.cantidad = quantity;
-     
-      setProduct(data);
-      console.log(data);
-     
-
-    
+    const newQuantity = quantity + 1;
+    setQuantity(newQuantity);
+    updateQuantity(newQuantity);
   };
 
   return (
@@ -47,37 +42,33 @@ export const ProductItemCart = ({ data }) => {
           <p className="item__text">{data.nombre}</p>
         </div>
 
-
         <div className="item__quantity">
-
-        {quantity > 1 &&
-         <RemoveIcon
-            style={{ fontSize: "25px" }}
-            className="item__quantity--minus"
-            onClick={handleDecrementQuantity}
-          />
-        }
-{quantity == 1 &&
-   <RemoveIcon
-   style={{ fontSize: "25px", color: "#EBEBEB" }}
-   className="item__quantity--minus"
-   onClick={handleDecrementQuantity}
- />
-}
-         
+          {quantity > 1 && (
+            <RemoveIcon
+              style={{ fontSize: "25px" }}
+              className="item__quantity--minus"
+              onClick={handleDecrementQuantity}
+            />
+          )}
+          {quantity === 1 && (
+            <RemoveIcon
+              style={{ fontSize: "25px", color: "#EBEBEB" }}
+              className="item__quantity--minus"
+              onClick={handleDecrementQuantity}
+            />
+          )}
           <p className="item__quantity--text">{quantity}</p>
           <AddIcon
             style={{ fontSize: "25px" }}
-            className="item__quantity--plus" 
+            className="item__quantity--plus"
             onClick={handleIncrementQuantity}
           />
         </div>
 
-        
         <div className="item__delete">
           <DeleteOutlineIcon
             style={{ fontSize: "30px" }}
-            onClick={() => removeItem(data.id, items)}
+            onClick={() => removeItem(data.id)}
           />
         </div>
       </section>
