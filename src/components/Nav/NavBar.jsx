92fe6466ade1da54b39d9342331282347b4b381d
MatiasRicci <matricc89@gmail.com>
2023-06-20 21:56:35 -0300

    llegoLaComida
    Descanso

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,7 +1,7 @@
 import "./NavBar.css";
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import LOGO from "../../assets/logo.png";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { ModalCart } from "../ModalCart/ModalCart";
@@ -18,8 +18,7 @@ const NavBar = () => {
     <header className="header">
       <div className="header__container">
         <div className="header__menu">
-
-        <MenuIcon/>
+          <MenuIcon />
         </div>
         {/* <div className="divOut"></div> */}
         <Link className="header__link" to={`/`}>
@@ -40,121 +39,131 @@ const NavBar = () => {
           {items.length > 0 && <div className="header__cart-indicator" />}
         </div>
       </div>
+      <div className="nav__header1">
+        
+        <nav className="header__nav">
+          <ul className="header__ul">
+            <li className="header__li">
+              <Link className="header__link" to={`/`}>
+                Inicio
+              </Link>
+            </li>
+            <li className="header__li">
+              <Link className="header__link" to={`/listaproductos/Herbo-Pets`}>
+                Herbo Pets
+              </Link>
+              <ul className="header__sub-ul">
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`/listaproductos/Herbo-Pets/Fórmulas-Florales-Pets`}
+                  >
+                    Formulas Florales
+                  </Link>
+                </li>
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`/listaproductos/Herbo-Pets/Brumas`}
+                  >
+                    Brumas
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="header__li">
+              <Link
+                className="header__link"
+                to={`/listaproductos/Sahumo-&-Defumación`}
+              >
+                Sahumos
+              </Link>
+            </li>
+            <li className="header__li">
+              <Link className="header__link" to={`/listaproductos/Herbo-Home`}>
+                Herbo Home
+              </Link>
+              <ul className="header__sub-ul">
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`/listaproductos/Herbo-Home/Sales-de-Baño`}
+                  >
+                    Sales de baño
+                  </Link>
+                </li>
 
-      <nav className="header__nav">
-        <ul className="header__ul">
-          <li className="header__li">
-            <Link className="header__link" to={`/`}>
-              Inicio
-            </Link>
-          </li>
-          <li className="header__li">
-            <Link className="header__link" to={`/listaproductos/Herbo-Pets`}>
-              Herbo Pets
-            </Link>
-            <ul className="header__sub-ul">
-              <li className="header__sub-li">
-                <Link className="header__link" to={`/listaproductos/Herbo-Pets/Fórmulas-Florales-Pets`}>
-                  Formulas Florales
-                </Link>
-              </li>
-              <li className="header__sub-li">
-                <Link className="header__link" to={`/listaproductos/Herbo-Pets/Brumas`}>
-                  Brumas
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li className="header__li">
-            <Link className="header__link" to={`/listaproductos/Sahumo-&-Defumación`}>
-              Sahumos
-            </Link>
-            {/* <ul className="header__sub-ul">
-              <li className="header__sub-li">
-                <Link className="header__link" to={`/category`}>
-                  Sahumerios
-                </Link>
-              </li>
-              <li className="header__sub-li">
-                <Link className="header__link" to={`/category`}>
-                  Carbones de defumacion
-                </Link>
-              </li>
-              <li className="header__sub-li">
-                <Link className="header__link" to={`/category`}>
-                  Velas
-                </Link>
-              </li>
-            </ul> */}
-          </li>
-          <li className="header__li">
-            <Link className="header__link" to={`/listaproductos/Herbo-Home`}>
-              Herbo Home
-            </Link>
-            <ul className="header__sub-ul">
-              <li className="header__sub-li">
-                <Link className="header__link" to={`/listaproductos/Herbo-Home/Sales-de-Baño`}>
-                  Sales de baño
-                </Link>
-              </li>
-              {/* <li className="header__sub-li">
-                <Link className="header__link" to={`/category`}>
-                  Difusores
-                </Link>
-              </li> */}
-              <li className="header__sub-li">
-                <Link className="header__link" to={`listaproductos/Herbo-Home/Esencias-para-hornillos`}>
-                  Esencias para hornillos
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li className="header__li">
-            <Link className="header__link" to={`listaproductos/Fórmulas-Florales-&-Tónicos-Herbales/Formulas-Florales`}>
-              Formula Floral
-            </Link>
-            <ul className="header__sub-ul">
-              <li className="header__sub-li">
-                <Link className="header__link" to={`listaproductos/Fórmulas-Florales-&-Tónicos-Herbales/Tónicos-Herbales`}>
-                  Tónicos herbales
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li className="header__li">
-            <Link className="header__link" to={`listaproductos/Aromaterapia`}>
-              Aromaterapias
-            </Link>
-            <ul className="header__sub-ul">
-              <li className="header__sub-li">
-                <Link className="header__link" to={`listaproductos/Aromaterapia/Body-oils`}>
-                  Body Oils
-                </Link>
-              </li>
-              <li className="header__sub-li">
-                <Link className="header__link" to={`listaproductos/Aromaterapia/Brumas-Aúricas`}>
-                  Brumas Auricas
-                </Link>
-              </li>
-              <li className="header__sub-li">
-                <Link className="header__link" to={`listaproductos/Aromaterapia/Roll-on`}>
-                  Roll On
-                </Link>
-              </li>
-              <li className="header__sub-li">
-                <Link className="header__link" to={`listaproductos/Aromaterapia/Aceites-Esceniales`}>
-                  Aceites esenciales
-                </Link>
-              </li>
-              {/* <li className="header__sub-li">
-                <Link className="header__link" to={`/category`}>
-                  Unguento
-                </Link>
-              </li> */}
-            </ul>
-          </li>
-        </ul>
-      </nav>
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`listaproductos/Herbo-Home/Esencias-para-hornillos`}
+                  >
+                    Esencias para hornillos
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="header__li">
+              <Link
+                className="header__link"
+                to={`listaproductos/Fórmulas-Florales-&-Tónicos-Herbales/Formulas-Florales`}
+              >
+                Formula Floral
+              </Link>
+              <ul className="header__sub-ul">
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`listaproductos/Fórmulas-Florales-&-Tónicos-Herbales/Tónicos-Herbales`}
+                  >
+                    Tónicos herbales
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li className="header__li">
+              <Link className="header__link" to={`listaproductos/Aromaterapia`}>
+                Aromaterapias
+              </Link>
+              <ul className="header__sub-ul">
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`listaproductos/Aromaterapia/Body-oils`}
+                  >
+                    Body Oils
+                  </Link>
+                </li>
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`listaproductos/Aromaterapia/Brumas-Aúricas`}
+                  >
+                    Brumas Auricas
+                  </Link>
+                </li>
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`listaproductos/Aromaterapia/Roll-on`}
+                  >
+                    Roll On
+                  </Link>
+                </li>
+                <li className="header__sub-li">
+                  <Link
+                    className="header__link"
+                    to={`listaproductos/Aromaterapia/Aceites-Esceniales`}
+                  >
+                    Aceites esenciales
+                  </Link>
+                </li>
+                
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
       <ModalCart show={show} handleClose={handleClose} />
     </header>
