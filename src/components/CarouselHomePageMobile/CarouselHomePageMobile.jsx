669813d2d877421f8ccf134d3a7265d3c1403853
BrianTDevCode <briantriasm@gmail.com>
2023-07-20 20:57:04 -0300

    prueba banner mobile

--- src/components/CarouselHomePageMobile/CarouselHomePageMobile.jsx
+++ src/components/CarouselHomePageMobile/CarouselHomePageMobile.jsx
@@ -6,33 +6,35 @@ import bannerMobile2 from "../../assets/bamnerMobile2.png";
 
 export const CarouselHomePageMobile = () => {
   return (
-    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-    <div className="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     </div>
-    <div className="carousel-inner">
-      <div className="carousel-item active">
-      <figure className="banner__figure">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+      <figure className="bannerfigure">
               <img
                 src={bannerMobile1}
-                className="d-block w-100  banner__img"
+                className="d-block w-100  bannerimg"
                 alt="banner"
               />
             </figure>
       </div>
-      <div className="carousel-item">
-      <figure className="banner__figure">
+      <div class="carousel-item">
+      <figure className="bannerfigure">
               <img
                 src={bannerMobile2}
-                className="d-block w-100 banner__img"
+                className="d-block w-100 bannerimg"
                 alt="banner"
               />
             </figure>
       </div>
-    
+
     </div>
-  
+
   </div>
+   
   );
 };
